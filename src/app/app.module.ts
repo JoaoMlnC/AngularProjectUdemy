355f86eeab603844493372b3148f02d0fab7733a
JoaoMlnC <joaomlncampos@hotmail.com>
2023-05-08 11:18:35 -0300

    updating project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ServerComponent } from './server/server.component';
-
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   
   ],
   providers: [],
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: 'servers.component.html', 
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+allowNewServer = false;
+serverCreationStatus = 'No server was created';
+serverName = 'TestServer';
+serverCreated= false;
 
+constructor() {
+  setTimeout(() => {
+    this.allowNewServer = true
+  }, 2000);
+}
+
+onCreateServer() {
+  this.serverCreated = true;
+  this.serverCreationStatus =  'Server was created! server name is ' + this.serverName;
+}
+
+onUpdateServerName(event: any) {
+this.serverName = (<HTMLInputElement>event.target).value;
+}
 }
